1999364840b4485a08717dd8b2c290023f704446
Wyatt C <cochranwyatt@yahoo.com>
2018-12-01 14:42:58 -0800

    Trying to get spotify API to work.

---
+++ keys.js
@@ -0,0 +1,17 @@
+console.log("This is loaded");
+var Spotify = require("node-spotify-api")
+var newSpotify = new Spotify({
+    id: "5c1d835f36e044f5ba6763a785b09b7b",
+    secret: "5f7ad49552c74500b75de62c772dd2f3"
+})
+
+exports.newSpotify 
+
+//exports.bandsintown = {
+  //  app_id: "7f0adce3b7216e5576b78e50ed53789a",
+//}
+
+//exports.omdb = {
+  //  apikey: "trilogy",
+    //url: "http://www.omdbapi.com/?apikey=" + apikey + "&t=" + userInput
+//}
